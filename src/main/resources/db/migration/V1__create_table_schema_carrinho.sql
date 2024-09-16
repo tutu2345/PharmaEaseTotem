41832ac52f9e391a015be40f636f608d508d3d0d
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2024-09-16 20:20:44 -0300

    fix: bugs fixed

--- src/main/resources/db/migration/V1__create_table_schema_carrinho.sql
+++ src/main/resources/db/migration/V1__create_table_schema_carrinho.sql
@@ -1,33 +1,14 @@
--- Tabela de Carrinhos
-CREATE TABLE Carrinho (
-                          id BIGINT IDENTITY(1,1) PRIMARY KEY,
-                          precoTotal DECIMAL(10, 2) NOT NULL,
-                          cliente_cpf BIGINT,
-                          FOREIGN KEY (cliente_cpf) REFERENCES Cliente(id)
-);
-
 -- Tabela de Clientes
 CREATE TABLE Cliente (
-                         id BIGINT IDENTITY(1,1) PRIMARY KEY,
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
                          nome NVARCHAR(255) NOT NULL,
                          email NVARCHAR(255) NOT NULL
-);
-
--- Tabela de Pedidos
-CREATE TABLE Pedido (
-                        pedido_id BIGINT IDENTITY(1,1) PRIMARY KEY,
-                        quantidade INT NOT NULL,
-                        preco DECIMAL(10, 2) NOT NULL,
-                        carrinho_id BIGINT,
-                        remedio_id BIGINT,
-                        FOREIGN KEY (carrinho_id) REFERENCES Carrinho(id),
-                        FOREIGN KEY (remedio_id) REFERENCES Remedio(id)
 );
 
 
 -- Tabela de Remédios
 CREATE TABLE Remedio (
-                         id BIGINT IDENTITY(1,1) PRIMARY KEY,
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
                          nome NVARCHAR(255) NOT NULL,
                          preco DECIMAL(10, 2) NOT NULL,
                          quantidade INT NOT NULL,
@@ -36,11 +17,23 @@ CREATE TABLE Remedio (
 
 
 
+-- Tabela de Carrinhos
+CREATE TABLE Carrinho (
+                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          precoTotal DECIMAL(10, 2) NOT NULL,
+                          cliente_cpf BIGINT,
+                          FOREIGN KEY (cliente_cpf) REFERENCES Cliente(id)
+);
 
 
-
-
-
-
-
+-- Tabela de Pedidos
+CREATE TABLE Pedido (
+                        pedido_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        quantidade INT NOT NULL,
+                        preco DECIMAL(10, 2) NOT NULL,
+                        carrinho_id BIGINT,
+                        remedio_id BIGINT,
+                        FOREIGN KEY (carrinho_id) REFERENCES Carrinho(id),
+                        FOREIGN KEY (remedio_id) REFERENCES Remedio(id)
+);
 
