90c5b11aca0c2c2ba8d422c277b68ca1dde5dc99
RamonReserva <ramoncezarino@hotmail.com>
2024-10-29 17:07:36 -0300

    mudançasna intermacionalização

--- src/main/java/br/com/pharmeeasetotem/totemapi/repository/CarrinhoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.pharmeeasetotem.totemapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.pharmeeasetotem.totemapi.model.Carrinho;
-
-public interface CarrinhoRepository extends JpaRepository<Carrinho, Integer> { }
--- src/main/java/br/com/pharmeeasetotem/totemapi/repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.pharmeeasetotem.totemapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.pharmeeasetotem.totemapi.model.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> { }
